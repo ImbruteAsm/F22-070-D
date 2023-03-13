968bfc254728e27d1203608e61e35cc9a0622e32
ImbruteAsm <117254304+ImbruteAsm@users.noreply.github.com>
2023-03-13 22:17:54 +0500

    Update inputpage.component.ts

--- FYP_FrontEnd/src/app/inputpage/inputpage.component.ts
+++ FYP_FrontEnd/src/app/inputpage/inputpage.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { Resolve } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Observable, timer } from 'rxjs';
+import { Domain } from 'src/app/Domain';
+import { DataService } from 'src/app/services/dataservice/data.service';
+import { DomainService } from 'src/app/services/domain/domain.service';
 
 @Component({
   selector: 'app-inputpage',
   templateUrl: './inputpage.component.html',
   styleUrls: ['./inputpage.component.css']
 })
-export class InputpageComponent {
-  public resolve(): Observable<number> {
-    return timer(7000);
-   }
+export class InputpageComponent{
+public pic1: string="assets/images/InputPicture.jpeg";
+constructor(private regDom: DomainService, private dataservice: DataService){}
+
+value=""
+getVal(val: any){
+  console.warn(val)
+  this.value= val
+  const urlvariable="http://localhost:9901/scan?target=";
+  this.value= urlvariable + this.value;
+  this.dataservice.InputData(this.value);
 }
+
+  }
+
+
